fd98b1f2367e5bb95afb544e7cbe073034ee41c9
Green Drake <wyrmling@users.noreply.github.com>
2017-06-23 19:45:34 +0300

    fixes

---
+++ main/characters/enemy.go
@@ -0,0 +1,35 @@
+components {
+  id: "script"
+  component: "/main/characters/enemy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/characters/char.tilesource\"\n"
+  "default_animation: \"pixie\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- utils/tools.lua
+++ utils/tools.lua
@@ -1,8 +1,15 @@
---
--- Created by IntelliJ IDEA.
--- User: Green Drake
--- Date: 21.06.2017
--- Time: 23:14
--- To change this template use File | Settings | File Templates.
---
-
+-- функция для копирования чего-либо, в основном таблиц, т.к. значения присваиваются по ссылке
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
+end
